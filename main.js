ab5128e40b83dac96ebc4a3b109f6af577006943
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-01-27 11:15:32 +0500

    ayruv cal

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var elInp = document.querySelector('#inputNumber');
 function calculator() {
     var selectValue = elSelect.value
     var inpValue = elInp.value
-    var result = inpValue * selectValue
+    var result = inpValue / selectValue
 
     h1.textContent = "Javob : " + result
 }
